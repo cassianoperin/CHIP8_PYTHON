2b2f21684709193dbdda6930b6354a6ba0e30ebb
Cassiano <cassianoperin@gmail.com>
2020-01-24 19:55:43 -0300

    Corrected rom path

--- menu.py
+++ menu.py
@@ -3,6 +3,7 @@ import sys
 import subprocess
 
 emulator="./chip8.py"
+rom_path="roms/"
 
 for i in range (0, 10):
     print ("\n")
@@ -39,127 +40,127 @@ game=input("\nType Game NUMBER:\n")
 if (game == "1"):
     print ("15PUZZLE")
     game = "15PUZZLE"
-    args=emulator + " " + game
+    args=emulator + " " + rom_path + game
     subprocess.call(args, shell=True)
 elif (game == "2"):
     print ("BLINKY")
     game = "BLINKY"
-    args=emulator + " " + game
+    args=emulator + " " + rom_path + game
     subprocess.call(args, shell=True)
 elif (game == "3"):
     print ("BLITZ")
     game = "BLITZ"
-    args=emulator + " " + game
+    args=emulator + " " + rom_path + game
     subprocess.call(args, shell=True)
 elif (game == "4"):
     print ("BRIX")
     game = "BRIX"
-    args=emulator + " " + game
+    args=emulator + " " + rom_path + game
     subprocess.call(args, shell=True)
 elif (game == "5"):
     print ("CONNECT4")
     game = "CONNECT4"
-    args=emulator + " " + game
+    args=emulator + " " + rom_path + game
     subprocess.call(args, shell=True)
 elif (game == "6"):
     print ("GUESS")
     game = "GUESS"
-    args=emulator + " " + game
+    args=emulator + " " + rom_path + game
     subprocess.call(args, shell=True)
 elif (game == "7"):
     print ("HIDDEN")
     game = "HIDDEN"
-    args=emulator + " " + game
+    args=emulator + " " + rom_path + game
     subprocess.call(args, shell=True)
 elif (game == "8"):
     print ("INVADERS")
     game = "INVADERS"
-    args=emulator + " " + game
+    args=emulator + " " + rom_path + game
     subprocess.call(args, shell=True)
 elif (game == "9"):
     print ("KALEID")
     game = "KALEID"
-    args=emulator + " " + game
+    args=emulator + " " + rom_path + game
     subprocess.call(args, shell=True)
 elif (game == "10"):
     print ("MAZE")
     game = "MAZE"
-    args=emulator + " " + game
+    args=emulator + " " + rom_path + game
     subprocess.call(args, shell=True)
 elif (game == "11"):
     print ("MERLIN")
     game = "MERLIN"
-    args=emulator + " " + game
+    args=emulator + " " + rom_path + game
     subprocess.call(args, shell=True)
 elif (game == "12"):
     print ("MISSILE")
     game = "MISSILE"
-    args=emulator + " " + game
+    args=emulator + " " + rom_path + game
     subprocess.call(args, shell=True)
 elif (game == "13"):
     print ("PONG")
     game = "PONG"
-    args=emulator + " " + game
+    args=emulator + " " + rom_path + game
     subprocess.call(args, shell=True)
 elif (game == "14"):
     print ("PONG2")
     game = "PONG2"
-    args=emulator + " " + game
+    args=emulator + " " + rom_path + game
     subprocess.call(args, shell=True)
 elif (game == "15"):
     print ("PUZZLE")
     game = "PUZZLE"
-    args=emulator + " " + game
+    args=emulator + " " + rom_path + game
     subprocess.call(args, shell=True)
 elif (game == "16"):
     print ("SYZYGY")
     game = "SYZYGY"
-    args=emulator + " " + game
+    args=emulator + " " + rom_path + game
     subprocess.call(args, shell=True)
 elif (game == "17"):
     print ("TANK")
     game = "TANK"
-    args=emulator + " " + game
+    args=emulator + " " + rom_path + game
     subprocess.call(args, shell=True)
 elif (game == "18"):
     print ("TETRIS")
     game = "TETRIS"
-    args=emulator + " " + game
+    args=emulator + " " + rom_path + game
     subprocess.call(args, shell=True)
 elif (game == "19"):
     print ("TICTAC")
     game = "TICTAC"
-    args=emulator + " " + game
+    args=emulator + " " + rom_path + game
     subprocess.call(args, shell=True)
 elif (game == "20"):
     print ("UFO")
     game = "UFO"
-    args=emulator + " " + game
+    args=emulator + " " + rom_path + game
     subprocess.call(args, shell=True)
 elif (game == "21"):
     print ("VBRIX")
     game = "VBRIX"
-    args=emulator + " " + game
+    args=emulator + " " + rom_path + game
     subprocess.call(args, shell=True)
 elif (game == "22"):
     print ("VERS")
     game = "VERS"
-    args=emulator + " " + game
+    args=emulator + " " + rom_path + game
     subprocess.call(args, shell=True)
 elif (game == "23"):
     print ("WIPEOFF")
     game = "WIPEOFF"
-    args=emulator + " " + game
+    args=emulator + " " + rom_path + game
     subprocess.call(args, shell=True)
 elif (game == "24"):
     print ("PONG3")
     game = "pong.ch8"
-    args=emulator + " " + game
+    args=emulator + " " + rom_path + game
     subprocess.call(args, shell=True)
 elif (game == "2"):
     print ("BLINKY")
     game = "BLINKY"
-    args=emulator + " " + game
+    args=emulator + " " + rom_path + game
     subprocess.call(args, shell=True)
 else:
     print ("\nGame not found.\nExiting.\n")
